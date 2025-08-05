dc90e84cf4112987001c9be61c6ccd95988ae337
Payel21 <126647062+Payel21@users.noreply.github.com>
2025-08-05 20:32:23 +0530

    Update and rename sscript.js to script.js

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ document.addEventListener('DOMContentLoaded', () => {
         surpriseContent.classList.remove('hidden');
         revealBtn.style.display = 'none'; // Hide the button after it's clicked
     });
+
 });
